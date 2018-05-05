63b91ba5c2adaacfa901563a6d412439f7b34ca7
alehuo <aleksi.huotala@helsinki.fi>
2018-05-05 17:03:26 +0300

    Enhancements to server handling. Upgraded some dependencies & added USERSERVICE_PORT env to .env.example

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as Knex from "knex";
@@ -8,6 +9,8 @@ import UserController from "./controllers/UserController";
 import UserService from "./services/UserService";
 
 const app = express();
+
+// Body parser
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -15,25 +18,45 @@ app.use(
   })
 );
 
+// Set static folder
 app.use(express.static("./public"));
+// Set views folder
 app.set("views", "./public/views");
+// Set view engine
 app.set("view engine", "pug");
 
+// Knexfile
 const knexfile = require("./../knexfile");
 
+// Knex instance
 const knex = Knex(knexfile[process.env.NODE_ENV || "staging"]);
 
-let authService = new AuthenticationService(knex);
-let userService = new UserService(knex);
+// Auth & user service
+const authService = new AuthenticationService(knex);
+const userService = new UserService(knex);
 
 // Routes
 const authController = new AuthController(authService, userService);
-let userController = new UserController(userService, authService);
+const userController = new UserController(userService, authService);
 
+// API routes
 app.use("/api/auth", authController.createRoutes());
 app.use("/api/users", userController.createRoutes());
+
+// Test login page
 app.get("/", (req, res) => {
   return res.render("login");
 });
 
-app.listen(3000);
+// Start server
+app.listen(process.env.USERSERVICE_PORT || 3000, () => {
+  console.log(
+    "User service listening on port %d",
+    process.env.USERSERVICE_PORT || 3000
+  );
+});
+
+process.on("SIGINT", () => {
+  console.log("Caught interrupt signal, server stopped");
+  process.exit();
+});
