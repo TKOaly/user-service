d99aef8bb8995384fe4134f9ce59485294e4df25
alehuo <aleksi.huotala@helsinki.fi>
2018-05-27 21:09:37 +0300

    Make production, staging, dev and test use different databases. Created DB creation script

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,8 @@
 // Update with your config settings.
 
-require('dotenv').config(); 
+require("dotenv").config();
 
-const mysql = {
+const prod = {
   client: "mysql2",
   connection: {
     host: process.env.AUTHSERVICE_DB_HOST,
@@ -18,11 +18,63 @@ const mysql = {
   migrations: {
     tableName: "knex_migrations"
   }
-}
+};
+const staging = {
+  client: "mysql2",
+  connection: {
+    host: process.env.AUTHSERVICE_DB_HOST,
+    port: process.env.AUTHSERVICE_DB_PORT,
+    user: process.env.AUTHSERVICE_DB_USER,
+    password: process.env.AUTHSERVICE_DB_PASSWORD,
+    database: process.env.AUTHSERVICE_DB_NAME + "_staging"
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: "knex_migrations"
+  }
+};
+const dev = {
+  client: "mysql2",
+  connection: {
+    host: process.env.AUTHSERVICE_DB_HOST,
+    port: process.env.AUTHSERVICE_DB_PORT,
+    user: process.env.AUTHSERVICE_DB_USER,
+    password: process.env.AUTHSERVICE_DB_PASSWORD,
+    database: process.env.AUTHSERVICE_DB_NAME + "_dev"
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: "knex_migrations"
+  }
+};
+
+const test = {
+  client: "mysql2",
+  connection: {
+    host: process.env.AUTHSERVICE_DB_HOST,
+    port: process.env.AUTHSERVICE_DB_PORT,
+    user: process.env.AUTHSERVICE_DB_USER,
+    password: process.env.AUTHSERVICE_DB_PASSWORD,
+    database: process.env.AUTHSERVICE_DB_NAME + "_test"
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: "knex_migrations"
+  }
+};
 
 module.exports = {
-  development: mysql,
-  staging: mysql,
-  production: mysql,
-  test: mysql
+  development: dev,
+  staging: staging,
+  production: prod,
+  test: test
 };
---
+++ scripts/create_databases.ts
@@ -0,0 +1,58 @@
+// This script creates databases that are needed by the user service.
+
+require("dotenv").config();
+
+import { createConnection, QueryError } from "mysql2/promise";
+
+const dbName: string = process.env.AUTHSERVICE_DB_NAME;
+
+async function createTables() {
+  // Create connection
+  console.log("Creating connection..");
+  const con = await createConnection({
+    host: process.env.AUTHSERVICE_DB_HOST,
+    port: process.env.AUTHSERVICE_DB_PORT,
+    user: process.env.AUTHSERVICE_DB_USER,
+    password: process.env.AUTHSERVICE_DB_PASSWORD
+  });
+  console.log("Connection created.");
+
+  try {
+    // Create databases
+    console.log("Creating production database named %s", dbName);
+    await con.execute(
+      "CREATE DATABASE IF NOT EXISTS " +
+        dbName +
+        " DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci"
+    );
+    console.log("Creating staging database named %s", dbName + "_staging");
+    await con.execute(
+      "CREATE DATABASE IF NOT EXISTS " +
+        dbName +
+        "_staging" +
+        " DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci"
+    );
+    console.log("Creating development database named %s", dbName + "_dev");
+    await con.execute(
+      "CREATE DATABASE IF NOT EXISTS " +
+        dbName +
+        "_dev" +
+        " DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci"
+    );
+
+    console.log("Creating testing database named %s", dbName + "_test");
+    await con.execute(
+      "CREATE DATABASE IF NOT EXISTS " +
+        dbName +
+        "_test" +
+        " DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci"
+    );
+    console.log("Created development, test, stating and production databases.");
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Run script
+createTables();
