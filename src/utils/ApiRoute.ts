ffd9faefa6231b6b8a27a9d883c488042c810895
alehuo <aleksi.huotala@helsinki.fi>
2018-05-27 15:40:29 +0300

    Added API route middleware

--- src/utils/ApiRoute.ts
+++ src/utils/ApiRoute.ts
@@ -1,3 +1,5 @@
+import * as Express from "express";
+
 /**
  * Generates an API route in the format /api/{API_VERSION}/{ENDPOINT_NAME} or /api/{ENDPOINT_NAME} depending on if API version is not set.
  *
@@ -6,10 +8,47 @@
  * @param {string} [apiVersion] API version. Defaults to null, can be configured manually.
  * @returns {string} API route, example: /api/v1/users
  */
-export function apiRoute(endpointName: string, apiVersion?: string): string {
+export function generateApiRoute(
+  endpointName: string,
+  apiVersion?: string
+): string {
   if (!apiVersion) {
     return "/api/" + endpointName;
   } else {
     return "/api/" + apiVersion + "/" + endpointName;
   }
+}
+/**
+ * API header middleware that sets headers.
+ *
+ * @param {string} [apiVersion] API version
+ * @returns
+ */
+export function apiHeaderMiddleware(apiVersion?: string) {
+  return function(
+    req: Express.Request,
+    res: Express.Response,
+    next: Express.NextFunction
+  ) {
+    if (apiVersion) {
+      res.setHeader("X-Route-API-version", apiVersion);
+    }
+    res.setHeader("X-API-version", process.env.API_VERSION);
+    next();
+  };
+}
+
+/**
+ * API route middleware.
+ *
+ * @export
+ * @param {string} endpointName Endpoint name
+ * @param {string} [apiVersion] API version
+ * @returns
+ */
+export function apiRouteMiddleware(endpointName: string, apiVersion?: string) {
+  return [
+    generateApiRoute(endpointName, apiVersion),
+    apiHeaderMiddleware(apiVersion)
+  ];
 }
