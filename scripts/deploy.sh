768383faa83114e7ca817015fd89ff94f5c5a31e
alehuo <aleksi.huotala@helsinki.fi>
2018-06-11 09:00:07 +0300

    Added more debug messages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
-set -x # Show the output of the following commands (useful for debugging)
+echo "=== User service deployment script ==="
     
 # Import the SSH deployment key
 if [ "$1" = “production” ]
 then
+    echo "Decrypting production environment SSH key"
     openssl aes-256-cbc -K $encrypted_22009518e18d_key -iv $encrypted_22009518e18d_iv -in .travis/deploy-key.enc.prod -out deploy.key -d
 else
+    echo "Decrypting staging environment SSH key"
     openssl aes-256-cbc -K $encrypted_22009518e18d_key -iv $encrypted_22009518e18d_iv -in .travis/deploy-key.enc.staging -out deploy.key -d
 fi
 
+echo "Setting up SSH key"
 rm deploy-key.enc.* # Don't need it anymore
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
 
+echo "Starting SSH connection"
 if [ "$1" = “production” ]
 then
- ssh ${PROD_SERVER_URL}
+    echo "Connecting to production server"
+    ssh ${PROD_SERVER_URL}
 else
- ssh ${STAGING_SERVER_URL}
+    echo "Connecting to staging server"
+    ssh ${STAGING_SERVER_URL}
 fi
 
 cd /srv/user-service
 
 if [ "$1" = “production” ]
 then
- git pull origin master
- git reset --hard origin/master
+    echo "Pulling latest code from master branch"
+    git pull origin master
+    git reset --hard origin/master
 else
- git pull origin dev
- git reset --hard origin/dev
+    echo "Pulling latest code from dev branch"
+    git pull origin dev
+    git reset --hard origin/dev
 fi
 
+echo "Building docker image"
 docker build -t user-service .
+
+echo "Starting docker image"
 docker run -p ${PORT}:${PORT} --env-file=.env -d --name user-service-container user-service
