8546b37175391ff681e8c09f7e3efc7f8d29e8c8
MergHQ <hugo.holm@live.com>
2018-06-11 19:31:06 +0300

    Moar attempts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ then
     openssl aes-256-cbc -K ${encrypted_26418d32bdf1_key} -iv ${encrypted_26418d32bdf1_iv} -in .travis/deploy-key.enc.prod -out deploy.key -d
 else
     echo "Decrypting staging environment SSH key"
-    openssl aes-256-cbc -K ${encrypted_22009518e18d_key} -iv ${encrypted_22009518e18d_iv} -in .travis/deploy-key.enc.staging -out deploy.key -d
+    openssl aes-256-cbc -K ${encrypted_26418d32bdf1_key} -iv ${encrypted_26418d32bdf1_iv} -in .travis/deploy-key.enc.staging -out deploy.key -d
 fi
 
 echo "Setting up SSH key"
