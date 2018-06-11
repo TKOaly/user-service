cb97d6b87377c421547dce6f1de0024430569d45
MergHQ <hugo.holm@live.com>
2018-06-11 19:35:33 +0300

    Attempt key encryption in before_install

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,14 @@
 echo "=== User service deployment script ==="
     
 # Import the SSH deployment key
-if [ "$1" = "production" ]
-then
-    echo "Decrypting production environment SSH key"
-    openssl aes-256-cbc -K ${encrypted_26418d32bdf1_key} -iv ${encrypted_26418d32bdf1_iv} -in .travis/deploy-key.enc.prod -out deploy.key -d
-else
-    echo "Decrypting staging environment SSH key"
-    openssl aes-256-cbc -K ${encrypted_26418d32bdf1_key} -iv ${encrypted_26418d32bdf1_iv} -in .travis/deploy-key.enc.staging -out deploy.key -d
-fi
+#if [ "$1" = "production" ]
+#then
+#    echo "Decrypting production environment SSH key"
+#    openssl aes-256-cbc -K $encrypted_22009518e18d_key -iv $encrypted_22009518e18d_iv -in .travis/deploy-key.enc.prod -out deploy.key -d
+#else
+#    echo "Decrypting staging environment SSH key"
+#    openssl aes-256-cbc -K $encrypted_22009518e18d_key -iv $encrypted_22009518e18d_iv -in .travis/deploy-key.enc.staging -out deploy.key -d
+#fi
 
 echo "Setting up SSH key"
 rm deploy-key.enc.* # Don't need it anymore
