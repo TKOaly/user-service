4995b679e5a6b8192b850473733d3c7b71d685b2
alehuo <aleksi.huotala@helsinki.fi>
2018-06-17 22:07:40 +0300

    Add tslint.json & Fix App.ts

--- src/App.ts
+++ src/App.ts
@@ -1,20 +1,20 @@
 require("dotenv").config();
 
+import * as cookieParser from "cookie-parser";
 import * as express from "express";
+import * as session from "express-session";
 import * as Knex from "knex";
 import AuthController from "./controllers/AuthController";
-import { AuthenticationService } from "./services/AuthenticationService";
-import UserController from "./controllers/UserController";
-import UserService from "./services/UserService";
-import * as session from "express-session";
-import * as cookieParser from "cookie-parser";
 import LoginController from "./controllers/LoginController";
-import UserDao from "./dao/UserDao";
-import ServiceDao from "./dao/ServiceDao";
-import ApiRoute from "./utils/ApiRoute";
-import PaymentService from "./services/PaymentService";
-import PaymentDao from "./dao/PaymentDao";
 import PaymentController from "./controllers/PaymentController";
+import UserController from "./controllers/UserController";
+import PaymentDao from "./dao/PaymentDao";
+import ServiceDao from "./dao/ServiceDao";
+import UserDao from "./dao/UserDao";
+import { AuthenticationService } from "./services/AuthenticationService";
+import PaymentService from "./services/PaymentService";
+import UserService from "./services/UserService";
+import ApiRoute from "./utils/ApiRoute";
 
 // Express application instance
 const app: express.Application = express();
@@ -30,10 +30,10 @@ app.use(cookieParser());
 app.set("trust proxy", 1); // trust first proxy
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
+    cookie: { secure: "auto", maxAge: 60000 },
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: "auto", maxAge: 60000 }
+    secret: process.env.SESSION_SECRET
   })
 );
 
