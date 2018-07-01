36bd6221e27ae501cdea9fa563d4d9a08a45ce02
alehuo <aleksi.huotala@helsinki.fi>
2018-07-01 17:16:37 +0300

    Fix linting error

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,11 +1,11 @@
-export const stringToBoolean: (str: any) => boolean = str => {
+export const stringToBoolean: (str: any) => boolean = (str) => {
   return str === "0"
-  ? false
-  : str === "false"
-  ? false
-  : str === "1"
-  ? true
-  : str === "true"
-  ? true
-  : false 
-}
+    ? false
+    : str === "false"
+      ? false
+      : str === "1"
+        ? true
+        : str === "true"
+          ? true
+          : false;
+};
