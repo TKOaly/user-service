73f718ec65899b1439f70036a44425230b268ddd
alehuo <aleksi.huotala@helsinki.fi>
2018-08-19 13:10:03 +0300

    Revamped e2e tests to use Mocha
    Fixed typo in error template. Added additional test cases for missing service identifier. Service identifier is now an optional parameter for language selection.
    
    Removed browser test script

---
+++ test/e2e/PrivacyPolicyPage.test.ts
@@ -0,0 +1,178 @@
+process.env.NODE_ENV = "test";
+
+import { By } from "selenium-webdriver";
+import { cleanupDriver, prepareDriver } from "../WebDriver";
+
+import chai = require("chai");
+import * as Knex from "knex";
+import "mocha";
+
+// Knexfile
+import * as knexfile from "../../knexfile";
+import { IKnexFile } from "../../knexfile";
+import { IServiceDatabaseObject } from "../../src/models/Service";
+
+import { WebDriver } from "selenium-webdriver";
+import services = require("../../seeds/seedData/services");
+
+import { Server } from "http";
+import app from "../../src/App";
+
+// @ts-ignore
+const should: Chai.Should = chai.should();
+
+// Knex instance
+const knex: Knex = Knex((knexfile as IKnexFile).test);
+
+const serviceData: IServiceDatabaseObject[] = services as IServiceDatabaseObject[];
+
+const en: any = require("./../../locales/en.json");
+const fi: any = require("./../../locales/fi.json");
+
+const port: number = 3010;
+
+describe("Privacy policy page", () => {
+  let browser: WebDriver;
+  let express: Server;
+
+  before((done: Mocha.Done) => {
+    prepareDriver().then((driver: WebDriver) => {
+      browser = driver;
+      express = app.listen(port, () => {
+        done();
+      });
+    });
+  });
+
+  after((done: Mocha.Done) => {
+    cleanupDriver(browser).then(() => {
+      express.close(() => {
+        done();
+      });
+    });
+  });
+
+  beforeEach((done: Mocha.Done) => {
+    // The before hook ensures, that knex runs database migrations and seeds.
+    knex.migrate.rollback().then(() => {
+      knex.migrate.latest().then(() => {
+        knex.seed.run().then(() => {
+          done();
+        });
+      });
+    });
+  });
+
+  afterEach((done: Mocha.Done) => {
+    knex.migrate.rollback().then(() => {
+      done();
+    });
+  });
+
+  for (const service of serviceData) {
+    it(
+      "On successful login, privacy policy page is shown for new users (Finnish) - " +
+        service.display_name,
+      async () => {
+        await browser.get(
+          "http://localhost:3010/lang/fi/" + service.service_identifier
+        );
+        await browser.findElement(By.id("username")).sendKeys("admin_user");
+        await browser.findElement(By.id("password")).sendKeys("admin_user");
+        await browser.findElement(By.className("accept")).click();
+
+        const containerTitle: string = await browser
+          .findElement(By.id("title"))
+          .getText();
+        containerTitle.should.equal(
+          service.display_name + " " + fi.privacypolicy_Title
+        );
+
+        const title: string = await browser.getTitle();
+        title.should.equal(
+          service.display_name + " " + fi.privacypolicy_Title + " - TKO-äly ry"
+        );
+
+        const cancelVal: string = await browser
+          .findElement(By.className("cancel"))
+          .getAttribute("value");
+        cancelVal.should.equal(fi.privacypolicy_Decline);
+
+        const acceptVal: string = await browser
+          .findElement(By.className("accept"))
+          .getAttribute("value");
+        acceptVal.should.equal(fi.privacypolicy_Accept);
+
+        const privacyPolicyRedirect: string = await browser
+          .findElement(By.className("privacyPolicyRedirect"))
+          .getText();
+        privacyPolicyRedirect.should.equal(
+          fi.privacypolicy_YouWillBeRedirected
+        );
+
+        const privacyPolicyDeclined: string = await browser
+          .findElement(By.className("privacyPolicyDeclineMessage"))
+          .getText();
+        privacyPolicyDeclined.should.equal(
+          fi.privacypolicy_IfYouDecline_1 +
+            " " +
+            service.display_name +
+            fi.privacypolicy_IfYouDecline_2
+        );
+      }
+    );
+  }
+
+  for (const service of serviceData) {
+    it(
+      "On successful login, privacy policy page is shown for new users (English) - " +
+        service.display_name,
+      async () => {
+        await browser.get(
+          "http://localhost:3010/lang/en/" + service.service_identifier
+        );
+        await browser.findElement(By.id("username")).sendKeys("admin_user");
+        await browser.findElement(By.id("password")).sendKeys("admin_user");
+        await browser.findElement(By.className("accept")).click();
+
+        const containerTitle: string = await browser
+          .findElement(By.id("title"))
+          .getText();
+        containerTitle.should.equal(
+          service.display_name + en.privacypolicy_Title
+        );
+
+        const title: string = await browser.getTitle();
+        title.should.equal(
+          service.display_name + en.privacypolicy_Title + " - TKO-äly ry"
+        );
+
+        const cancelVal: string = await browser
+          .findElement(By.className("cancel"))
+          .getAttribute("value");
+        cancelVal.should.equal(en.privacypolicy_Decline);
+
+        const acceptVal: string = await browser
+          .findElement(By.className("accept"))
+          .getAttribute("value");
+        acceptVal.should.equal(en.privacypolicy_Accept);
+
+        const privacyPolicyRedirect: string = await browser
+          .findElement(By.className("privacyPolicyRedirect"))
+          .getText();
+        privacyPolicyRedirect.should.equal(
+          en.privacypolicy_YouWillBeRedirected
+        );
+
+        const privacyPolicyDeclined: string = await browser
+          .findElement(By.className("privacyPolicyDeclineMessage"))
+          .getText();
+        privacyPolicyDeclined.should.equal(
+          en.privacypolicy_IfYouDecline_1 +
+            service.display_name +
+            en.privacypolicy_IfYouDecline_2
+        );
+      }
+    );
+  }
+}).timeout(5000);
