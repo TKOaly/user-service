44724bdf1b3723b7fbf64f1c579c50dfafcf70b1
alehuo <aleksi.huotala@helsinki.fi>
2018-10-06 18:20:28 +0300

    Updated deploy script

--- deploy-container.sh
+++ deploy-container.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Pushing container image to registry.tko-aly.fi"
-source .env.deploy
 echo $DEPLOY_PASSWORD | docker login registry.tko-aly.fi -u $DEPLOY_USERNAME --password-stdin
 docker build . -t user-service
 docker tag user-service:latest registry.tko-aly.fi/user-service:latest
