95866d9060a97818f21fbd5471354cde6b5bc641
Sami Kukkonen <sami@samikukkonen.fi>
2018-10-12 20:14:59 +0300

    Increase /dev/shm size for deploy stage

--- deploy-container.sh
+++ deploy-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Pushing container image to registry.tko-aly.fi"
 echo $DEPLOY_PASSWORD | docker login registry.tko-aly.fi -u $DEPLOY_USERNAME --password-stdin
-docker build . -t user-service
+docker build . -t user-service --shm-size 1G
 docker tag user-service:latest registry.tko-aly.fi/user-service:latest
 docker push registry.tko-aly.fi/user-service:latest
