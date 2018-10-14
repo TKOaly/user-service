7e5a91852416bd28bd52dac3bd2bb1d683007a40
Sami Kukkonen <sami@samikukkonen.fi>
2018-10-14 17:56:17 +0300

    Try out remote docker integration from Travis

--- deploy-container.sh
+++ deploy-container.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+set -e
 echo "Pushing container image to registry.tko-aly.fi"
 echo $DEPLOY_PASSWORD | docker login registry.tko-aly.fi -u $DEPLOY_USERNAME --password-stdin
 docker build . -t user-service --shm-size 1G
 docker tag user-service:latest registry.tko-aly.fi/user-service:latest
 docker push registry.tko-aly.fi/user-service:latest
+
+echo "Deploying staging"
+echo "Decrypting private key..."
+openssl aes256-cbc -a -d -pass env:DOCKER_KEY_PASSPHRASE -in deploy-keys/key.pem.enc -out deploy-keys/key.pem
+
+DOCKER_CERT_PATH="deploy-keys"
+DOCKER_TLS=1
+DOCKER_TLS_VERIFY=1
+DOCKER_HOST="uolevi.tko-aly.fi:2376"
+
+docker service ls
+
+echo "Destroying private key..."
+rm deploy-keys/key.pem
