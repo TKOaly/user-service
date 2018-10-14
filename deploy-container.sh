efb3ea317abafbc2396f18f05be47c326611e41a
Sami Kukkonen <sami@samikukkonen.fi>
2018-10-14 18:34:03 +0300

    Enable CD to staging from Travis

--- deploy-container.sh
+++ deploy-container.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
-echo "Pushing container image to registry.tko-aly.fi"
+TAG=$TRAVIS_COMMIT
+echo "Pushing container image to registry.tko-aly.fi (tag=$TAG)"
 echo $DEPLOY_PASSWORD | docker login registry.tko-aly.fi -u $DEPLOY_USERNAME --password-stdin
 docker build . -t user-service --shm-size 1G
 docker tag user-service:latest registry.tko-aly.fi/user-service:latest
+docker tag user-service:latest registry.tko-aly.fi/user-service:$TAG
 docker push registry.tko-aly.fi/user-service:latest
+docker push registry.tko-aly.fi/user-service:$TAG
 
 echo "Deploying staging"
 echo "Decrypting private key..."
@@ -15,7 +18,7 @@ export DOCKER_TLS=1
 export DOCKER_TLS_VERIFY=1
 export DOCKER_HOST="uolevi.tko-aly.fi:2376"
 
-docker service ls
+docker service update --image registry.tko-aly.fi/user-service:$TAG user-service
 
 echo "Destroying private key..."
 rm deploy-keys/key.pem
