3c39e5a85df2d2b76ad55af84ef9e7245f0ebde5
Sami Kukkonen <sami@samikukkonen.fi>
2018-10-19 14:19:19 +0300

    Use deploy webhook instead of direct docker commands

--- deploy-container.sh
+++ deploy-container.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 set -e
 TAG=$TRAVIS_COMMIT
-echo "Pushing container image to registry.tko-aly.fi (tag=$TAG)"
+echo "Building Docker image..."
 echo $DEPLOY_PASSWORD | docker login registry.tko-aly.fi -u $DEPLOY_USERNAME --password-stdin
 docker build . -t user-service --shm-size 1G
 docker tag user-service:latest registry.tko-aly.fi/user-service:latest
 docker tag user-service:latest registry.tko-aly.fi/user-service:$TAG
+echo "Pushing image to registry..."
 docker push registry.tko-aly.fi/user-service:latest
 docker push registry.tko-aly.fi/user-service:$TAG
+echo "Image: registry.tko-aly.fi/user-service:$TAG"
 
-echo "Deploying staging"
-echo "Decrypting private key..."
-openssl aes-256-cbc -a -d -pass env:DOCKER_KEY_PASSPHRASE -in deploy-keys/key.pem.enc -out deploy-keys/key.pem
-
-export DOCKER_CERT_PATH="deploy-keys"
-export DOCKER_TLS=1
-export DOCKER_TLS_VERIFY=1
-export DOCKER_HOST="uolevi.tko-aly.fi:2376"
-
-docker service update --image registry.tko-aly.fi/user-service:$TAG user-service
-
-echo "Destroying private key..."
-rm deploy-keys/key.pem
+echo "Deploying to staging..."
+curl -XPOST http://uolevi.tko-aly.fi:9000/api/webhooks/6217411b-201c-42e7-b743-e0e07af39d07
+echo "OK"
