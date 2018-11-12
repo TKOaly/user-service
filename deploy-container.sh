39c131046eeac90628e43c2548748b25e0fc6210
MergHQ <hugo.holm@live.com>
2018-11-12 23:00:25 +0200

    Attempt to fix docker registry sign-in error

--- deploy-container.sh
+++ deploy-container.sh
@@ -2,7 +2,7 @@
 set -e
 TAG=$TRAVIS_COMMIT
 echo "Building Docker image..."
-docker login registry.tko-aly.fi -u $DEPLOY_USERNAME --password $DEPLOY_PASSWORD
+docker login registry.tko-aly.fi --username $DEPLOY_USERNAME --password $DEPLOY_PASSWORD
 docker build . -t user-service --shm-size 1G
 docker tag user-service:latest registry.tko-aly.fi/user-service:latest
 docker tag user-service:latest registry.tko-aly.fi/user-service:$TAG
