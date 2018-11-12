8f3f962bfee494cf83164a328b8bd168aa991eaf
alehuo <aleksi.huotala@helsinki.fi>
2018-11-12 22:26:05 +0200

    Revert "Remove registry password input via stdin"
    This reverts commit d9d58759335601d758d1ebbb47b52eaa41518f47.

--- deploy-container.sh
+++ deploy-container.sh
@@ -2,7 +2,7 @@
 set -e
 TAG=$TRAVIS_COMMIT
 echo "Building Docker image..."
-echo docker login https://registry.tko-aly.fi -u $DEPLOY_USERNAME -p $DEPLOY_PASSWORD
+echo $DEPLOY_PASSWORD | docker login registry.tko-aly.fi -u $DEPLOY_USERNAME --password-stdin
 docker build . -t user-service --shm-size 1G
 docker tag user-service:latest registry.tko-aly.fi/user-service:latest
 docker tag user-service:latest registry.tko-aly.fi/user-service:$TAG
