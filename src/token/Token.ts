b3e554cb98da9511e9e4601426fb8030d6553bbf
Sami Kukkonen <sami@samikukkonen.fi>
2018-12-24 21:15:23 +0200

    Cleanup Token

--- src/token/Token.ts
+++ src/token/Token.ts
@@ -1,32 +1,13 @@
 import * as JWT from "jsonwebtoken";
 import IParsedTokenContents from "../interfaces/IParsedTokenContents";
 
-/**
- * ServiceToken class.
- *
- * @export
- * @class ServiceToken
- */
 export default class ServiceToken {
-  /**
-   * Creates an instance of ServiceToken.
-   * @param {number} userId User id
-   * @param {string[]} authenticatedTo Service list
-   * @param {Date} createdAt Creation date
-   * @memberof ServiceToken
-   */
   constructor(
     public userId: number,
     public authenticatedTo: string[],
     public createdAt: Date
   ) {}
 
-  /**
-   * Converts the token to a string.
-   *
-   * @returns {string} Token as a string
-   * @memberof ServiceToken
-   */
   public toString(): string {
     try {
       const parsedTokenContents: IParsedTokenContents = {
@@ -41,13 +22,6 @@ export default class ServiceToken {
   }
 }
 
-/**
- * Converts a token string to a ServiceToken.
- *
- * @export
- * @param {string} token Token as string
- * @returns {ServiceToken}
- */
 export function stringToServiceToken(token: string): ServiceToken {
   let parsedToken: string | object = null;
   try {
