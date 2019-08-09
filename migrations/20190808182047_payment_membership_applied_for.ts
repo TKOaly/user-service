4d5295b3cff49a7bbd305a0d213e810939ab3a08
Aleksi Huotala <aleksi.huotala@helsinki.fi>
2019-08-09 17:44:20 +0300

    Add .sessions to .gitignore. Fix migration

--- migrations/20190808182047_payment_membership_applied_for.ts
+++ migrations/20190808182047_payment_membership_applied_for.ts
@@ -1,13 +1,13 @@
 import Knex from "knex";
 
 exports.up = async function(knex: Knex): Promise<void> {
-  return knex.schema.table("services", t => {
+  return knex.schema.table("payments", t => {
     t.string("membership_applied_for");
   });
 };
 
 exports.down = async function(knex: Knex): Promise<void> {
-  return knex.schema.table("services", t => {
+  return knex.schema.table("payments", t => {
     t.dropColumn("membership_applied_for");
   });
 };
