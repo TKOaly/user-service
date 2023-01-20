04d137a5671353860efd346dd7ac25a0f16778d0
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2023-01-21 01:03:15 +0200

    Don't insert user related seeds in .localhost

---
+++ seeds/2_services.ts
@@ -0,0 +1,43 @@
+import type Knex from "knex";
+import privacyPolicies from "./seedData/privacy_policies";
+import consentData from "./seedData/privacy_policy_consent_datas";
+const services = require("./seedData/services");
+
+exports.seed = async function (knex: Knex) {
+  if (process.env.NODE_ENV === "production") {
+    throw new Error("Please do not seed a production database.");
+  }
+
+  if (process.env.SEED_FOR_TKOALY_LOCALHOST === "true") {
+    // make an effort at seeding the services and privacy policies but just let it fail if they're already seeded
+    // or local dev work has produced conflicting IDs
+    //
+    // a fancier solution would probably match by uuid and skip those, and not have a hardcoded id in that case
+    for (const service of services) {
+      try {
+        await knex("services").insert([service]);
+      } catch (e) {
+        console.error(`Error while seeding service, skipping it...`, service);
+      }
+    }
+    for (const policy of privacyPolicies) {
+      try {
+        await knex("privacy_policies").insert([policy]);
+      } catch (e) {
+        console.error(`Error while seeding privacy policy, skipping it...`, policy);
+      }
+    }
+    return;
+  }
+
+  // only user-service dev - seed with ids
+
+  // Deletes ALL existing entries
+  await knex("privacy_policies").del();
+  await knex("privacy_policy_consent_data").del();
+  await knex("services").del();
+
+  await knex("services").insert(services);
+  await knex("privacy_policies").insert(privacyPolicies);
+  await knex("privacy_policy_consent_data").insert(consentData);
+};
