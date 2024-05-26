ea53d48b31851c4885480e0f1ace41701d193f6d
NuiS4ncE <nuis4nce@hotmail.com>
2024-05-27 01:17:28 +0300

    removed redundant console.logs, still problems in github actions with prettier

--- knexfile.ts
+++ knexfile.ts
@@ -44,11 +44,6 @@ export const production: Knex.Config = {
     directory: "do_not_seed_prod_db",
   },
 };
-console.log("DB_HOST: ", process.env.DB_HOST);
-console.log("DB_PORT: ", process.env.DB_PORT);
-console.log("DB_USER: ", process.env.DB_USER);
-console.log("DB_PASSWORD: ", process.env.DB_PASSWORD);
-console.log("DB_NAME: ", process.env.DB_NAME);
 
 export const staging: Knex.Config = {
   client: "mysql2",
