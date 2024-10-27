3469fc5f95f53698d552b5d1b301d50fb196d979
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2024-10-27 15:52:41 +0200

    Add --ignore-existing option

--- scripts/create_user.ts
+++ scripts/create_user.ts
@@ -24,7 +24,14 @@ const parseArgs = () =>
     "--is-hyy-member": Boolean,
     "--is-tktdt-student": Boolean,
     "--wait": Boolean,
+    "--ignore-existing": Boolean,
   });
+
+const CONFLICT_MESSAGES = [
+  "Failed to reserve ID for user!",
+  "Username already in use!",
+  "Email address already in use!"
+];
 
 const main = async () => {
   const args = parseArgs();
@@ -47,33 +54,45 @@ const main = async () => {
 
   const created = args["--created-at"] ? moment(args["--created-at"], "YYYY-MM-DD").toDate() : new Date();
 
-  await UserService.createUser(
-    new User({
-      username,
-      name,
-      screen_name,
-      email,
-      phone,
-      residence,
-      hyy_member: args["--is-hyy-member"] ? 1 : 0,
-      hy_staff: args["--is-hy-staff"] ? 1 : 0,
-      hy_student: args["--is-hy-student"] ? 1 : 0,
-      tktdt_student: args["--is-tktdt-student"] ? 1 : 0,
-      tktl: args["--is-tktl"] ? 1 : 0,
-      role: (args["--role"] as UserRoleString | undefined) ?? UserRoleString.Kayttaja,
-      membership: args["--membership"] ?? "ei-jasen",
-      created,
-      salt: "",
-      hashed_password: "",
-      password_hash: "",
-      last_seq: 0,
-      deleted: 0,
-      modified: new Date(),
-      id: args["--id"],
-    } as UserDatabaseObject),
-    password,
-    !!args["--wait"],
-  );
+  try {
+    await UserService.createUser(
+      new User({
+        username,
+        name,
+        screen_name,
+        email,
+        phone,
+        residence,
+        hyy_member: args["--is-hyy-member"] ? 1 : 0,
+        hy_staff: args["--is-hy-staff"] ? 1 : 0,
+        hy_student: args["--is-hy-student"] ? 1 : 0,
+        tktdt_student: args["--is-tktdt-student"] ? 1 : 0,
+        tktl: args["--is-tktl"] ? 1 : 0,
+        role: (args["--role"] as UserRoleString | undefined) ?? UserRoleString.Kayttaja,
+        membership: args["--membership"] ?? "ei-jasen",
+        created,
+        salt: "",
+        hashed_password: "",
+        password_hash: "",
+        last_seq: 0,
+        deleted: 0,
+        modified: new Date(),
+        id: args["--id"],
+      } as UserDatabaseObject),
+      password,
+      !!args["--wait"],
+    );
+  } catch (err) {
+    if (err instanceof Error && CONFLICT_MESSAGES.includes(err.message)) {
+      if (args["--ignore-existing"]) {
+        console.log(`Failed to create user: ${err.message}`);
+        console.log('Ignoring error because --ignore-existing is set.');
+        process.exit(0);
+      }
+    }
+
+    throw err;
+  }
 
   process.exit(0);
 };
